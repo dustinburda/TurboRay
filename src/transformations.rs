b2bf3aba26a47eecff38f7d1e4430b5b72d9e5f5
dustinburda <dustin99wii@gmail.com>
2024-01-18 13:55:29 -0500

    partial transformation test

--- src/transformations.rs
+++ src/transformations.rs
@@ -1,4 +1,5 @@
 use crate::matrix::{Matrix, Mat44};
+use std::f64::consts::PI;
 
 pub fn translation(Tx: f64, Ty: f64, Tz: f64) -> Mat44 {
     Matrix::new([[1.0, 0.0, 0.0, Tx],
@@ -19,10 +20,10 @@ pub fn scale(x: f64, y: f64, z:f64) -> Mat44 {
 * @param: r, degrees in radians
 */
 pub fn rotx(r: f64) -> Mat44 {
-    Matrix::new([[0.0, 0.0, 0.0, 0.0],
-                          [0.0, 0.0, 0.0, 0.0],
-                          [0.0, 0.0, 0.0, 0.0],
-                          [0.0, 0.0, 0.0, 0.0]])
+    Matrix::new([[1.0, 0.0,              0.0,                0.0],
+                          [0.0, f64::cos(r), -f64::sin(r), 0.0],
+                          [0.0, f64::sin(r), f64::cos(r),  0.0],
+                          [0.0, 0.0,               0.0,               1.0]])
 }
 
 
@@ -30,10 +31,10 @@ pub fn rotx(r: f64) -> Mat44 {
 * @param: r, degrees in radians
 */
 pub fn roty(r: f64) -> Mat44 {
-    Matrix::new([[0.0, 0.0, 0.0, 0.0],
-                          [0.0, 0.0, 0.0, 0.0],
-                          [0.0, 0.0, 0.0, 0.0],
-                          [0.0, 0.0, 0.0, 0.0]])
+    Matrix::new([[f64::cos(r),  0.0,  f64::sin(r), 0.0],
+                           [0.0,               1.0,  0.0,              0.0],
+                           [-f64::sin(r), 0.0, f64::cos(r), 0.0],
+                           [0.0,               0.0,  0.0,              1.0]])
 }
 
 
@@ -41,10 +42,10 @@ pub fn roty(r: f64) -> Mat44 {
 * @param: r, degrees in radians
 */
 pub fn rotz(r: f64) -> Mat44 {
-    Matrix::new([[0.0, 0.0, 0.0, 0.0],
-                           [0.0, 0.0, 0.0, 0.0],
-                           [0.0, 0.0, 0.0, 0.0],
-                           [0.0, 0.0, 0.0, 0.0]])
+    Matrix::new([[f64::cos(r), -f64::sin(r), 0.0, 0.0],
+                           [f64::sin(r), f64::cos(r), 0.0, 0.0],
+                           [0.0,               0.0,              1.0, 0.0],
+                           [0.0,               0.0,              0.0, 1.0]])
 }
 
 pub fn shear(x_z: f64, x_y: f64, y_x: f64, y_z: f64, z_x: f64, z_y: f64) -> Mat44 {
@@ -57,9 +58,137 @@ pub fn shear(x_z: f64, x_y: f64, y_x: f64, y_z: f64, z_x: f64, z_y: f64) -> Mat4
 
 #[cfg(test)]
 mod tests {
-    
+    use crate::transformations::{translation, scale, rotx, roty, rotz, shear};
+    use crate::vec::{Vec, Vec3};
+    use std::f64::consts::PI;
+    use crate::matrix::Matrix;
+
+    const EPSILON: f64 = 0.0001;
+    fn float_equal(x: f64, y: f64) -> bool {
+        f64::abs(x - y) < EPSILON
+    }
     #[test]
-    fn test1() {
-        assert_eq!(1,1);
+    fn translate_test() {
+        let p: Vec3 = Vec::new([-3.0, 4.0, 5.0]);
+
+        let translate1 = translation(5.0, -3.0, 2.0);
+
+        // Translation matrix should translate point
+        let p_translate = (translate1 * (p.homogenize())).dehomogenize();
+
+        assert!(float_equal(p_translate[0], 2.0));
+        assert!(float_equal(p_translate[1], 1.0));
+        assert!(float_equal(p_translate[2], 7.0));
+
+        // The inverse translation should transform the translated point to the original point
+        let inv_translate1 = translate1.inv();
+
+        let inv_p_translate = (inv_translate1 * p_translate.homogenize()).dehomogenize();
+
+        assert!(float_equal(inv_p_translate[0], -3.0));
+        assert!(float_equal(inv_p_translate[1], 4.0));
+        assert!(float_equal(inv_p_translate[2], 5.0));
+
+        // Translations don't affect vectors
+        let v: Vec3 = Vec::new([5.0, -3.0, 2.0]);
+
+        let v_translate = (translate1 * v.homogenize_vec()).dehomogenize();
+
+        assert!(float_equal(v_translate[0], 5.0));
+        assert!(float_equal(v_translate[1], -3.0));
+        assert!(float_equal(v_translate[2], 2.0));
+
+        
+    }
+
+    #[test]
+    fn scale_test() {
+        let p: Vec3 = Vec::new([-4.0, 6.0, 8.0]);
+
+        let scale1 = scale(2.0, 3.0, 4.0);
+
+        let p_scale = (scale1 * (p.homogenize())).dehomogenize();
+
+        assert!(float_equal(p_scale[0], -8.0));
+        assert!(float_equal(p_scale[1], 18.0));
+        assert!(float_equal(p_scale[2], 32.0));
+
+        let inv_scale1 = scale1.inv();
+
+        let inv_p_scale = (inv_scale1 * p.homogenize()).dehomogenize();
+
+        assert!(float_equal(inv_p_scale[0], -2.0));
+        assert!(float_equal(inv_p_scale[1], 2.0));
+        assert!(float_equal(inv_p_scale[2], 2.0));
+
+
+        let v: Vec3 = Vec::new([-4.0, 6.0, 8.0]);
+
+        let v_scale = (scale1 * v.homogenize_vec()).dehomogenize();
+
+        assert!(float_equal(v_scale[0], -8.0));
+        assert!(float_equal(v_scale[1], 18.0));
+        assert!(float_equal(v_scale[2], 32.0));
+    }
+
+    #[test]
+    fn rotate_test() {
+        let v1: Vec3 = Vec::new([0.0, 1.0, 0.0]);
+
+        let rotPI_4x = rotx(PI / 4.0);
+        let rotPI_2x = rotx(PI / 2.0);
+        
+        let v_x_pi_4 = (rotPI_4x * v1.homogenize()).dehomogenize();
+        assert!(float_equal(v_x_pi_4[0], 0.0));
+        assert!(float_equal(v_x_pi_4[1], f64::sqrt(2.0)/2.0));
+        assert!(float_equal(v_x_pi_4[2], f64::sqrt(2.0)/2.0));
+
+        let v_x_pi_2 = (rotPI_2x * v1.homogenize()).dehomogenize();
+        assert!(float_equal(v_x_pi_2[0], 0.0));
+        assert!(float_equal(v_x_pi_2[1], 0.0));
+        assert!(float_equal(v_x_pi_2[2], 1.0));
+
+        let inv_rotPI_4x = rotx(PI / 4.0).inv();
+        let inv_v_x_pi_4 =  (inv_rotPI_4x * v1.homogenize()).dehomogenize();
+        assert!(float_equal(inv_v_x_pi_4[0], 0.0));
+        assert!(float_equal(inv_v_x_pi_4[1], f64::sqrt(2.0)/2.0));
+        assert!(float_equal(inv_v_x_pi_4[2], -f64::sqrt(2.0)/2.0));
+
+
+        let v2: Vec3 = Vec::new([0.0, 0.0, 1.0]);
+
+        let rotPI_4y = roty(PI / 4.0);
+        let rotPI_2y = roty(PI / 2.0);
+
+        let v_y_pi_4 = (rotPI_4y * v2.homogenize()).dehomogenize();
+        assert!(float_equal(v_y_pi_4[0], f64::sqrt(2.0)/2.0));
+        assert!(float_equal(v_y_pi_4[1], 0.0));
+        assert!(float_equal(v_y_pi_4[2], f64::sqrt(2.0)/2.0));
+
+        let v_y_pi_2 = (rotPI_2y * v2.homogenize()).dehomogenize();
+        assert!(float_equal(v_y_pi_2[0], 1.0));
+        assert!(float_equal(v_y_pi_2[1], 0.0));
+        assert!(float_equal(v_y_pi_2[2], 0.0));
+
+
+        let rotPI_4z = rotz(PI / 4.0);
+        let rotPI_2z = rotz(PI / 2.0);
+        
+        let v_z_pi_4 = (rotPI_4z * v1.homogenize()).dehomogenize();
+        assert!(float_equal(v_z_pi_4[0], -f64::sqrt(2.0)/2.0));
+        assert!(float_equal(v_z_pi_4[1], f64::sqrt(2.0)/2.0));
+        assert!(float_equal(v_z_pi_4[2], 0.0));
+
+        let v_z_pi_2 = (rotPI_2z * v1.homogenize()).dehomogenize();
+        assert!(float_equal(v_z_pi_2[0], -1.0));
+        assert!(float_equal(v_z_pi_2[1], 0.0));
+        assert!(float_equal(v_z_pi_2[2], 0.0));
+    }
+
+
+    #[test]
+    fn shear_test() {
+        // let v1: Vec3 = Vec::new([1.])
+
     }
 }
